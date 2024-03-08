06919d767c322d3a2e5a496a95d4cbf6d9620213
André Moriya <andremoriya@gmail.com>
2024-03-08 11:37:06 +0000

    Updated UserService.java

--- src/main/java/me/moriya/service/UserService.java
+++ src/main/java/me/moriya/service/UserService.java
@@ -1,2 +1,23 @@
-package me.moriya.service;public class UserService {
+package me.moriya.service;
+
+import lombok.RequiredArgsConstructor;
+import me.moriya.entity.User;
+import me.moriya.mapper.UserMapper;
+import me.moriya.model.request.UserRequest;
+import me.moriya.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final UserMapper userMapper;
+
+    public Mono<User> save(final UserRequest userRequest) {
+        return userRepository.save(userMapper.toEntity(userRequest));
+    }
+
 }
--- src/test/java/me/moriya/WebfluxCourseApplicationTests.java
+++ src/test/java/me/moriya/WebfluxCourseApplicationTests.java
@@ -1,12 +1,9 @@
 package me.moriya;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+// @SpringBootTest
 class WebfluxCourseApplicationTests {
 
-    @Test
+    // @Test
     void contextLoads() {
     }
 
