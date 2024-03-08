82c41d9ac7f685fd77e2714c3c88745787fc211b
André Moriya <andremoriya@gmail.com>
2024-03-08 10:41:54 +0000

    Add user entity

---
+++ src/main/java/me/moriya/entity/User.java
@@ -0,0 +1,26 @@
+package me.moriya.entity;
+
+import jakarta.validation.constraints.Email;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Email
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+}
